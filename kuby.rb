a21326bafb9a57f119c8e71ceada2fafd10b10fa
Kingdon Barrett <kingdon@weave.works>
2021-12-11 00:00:19 -0500

    xxxInvalid - use ambient credentials docker/ghcr

--- kuby.rb
+++ kuby.rb
@@ -17,7 +17,7 @@ Kuby.define('KubyTest') do
     docker do
       credentials do
         username app_creds[:KUBY_DOCKER_USERNAME]
-        password app_creds[:KUBY_DOCKER_PASSWORD]
+        password ENV['GITHUB_TOKEN']
         email app_creds[:KUBY_DOCKER_EMAIL]
       end
 
